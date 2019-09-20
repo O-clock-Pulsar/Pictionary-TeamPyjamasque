2b8a3db1595a50252f41975d45e94d18e999ca3f
Nicolas Provoost <provoostnicolas@gmail.com>
2019-09-20 14:20:03 +0200

    Closes #63

---
+++ src/models/Picture.ts
@@ -0,0 +1,22 @@
+import {Schema, model, Document} from 'mongoose';
+
+export interface IPicture extends Document {
+    user_id : number,
+    picture : Buffer 
+}
+
+const pictureSchema = new Schema(
+    {
+        user_id : {
+            type : Number,
+            required : true,
+        },
+        picture : {
+            type : Buffer,
+            required : true,
+            unique : true,
+        }
+    }
+)
+
+export default model<IPicture>('Picture', pictureSchema);
