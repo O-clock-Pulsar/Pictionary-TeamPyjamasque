33d2d96daae97dcbccd1a0d4fa8b629ff1125ce8
AdamWier <38018787+AdamWier@users.noreply.github.com>
2019-11-15 13:37:42 +0100

    Other socketio implementation Closes #111

--- src/services/SocketIOServer.ts
+++ src/services/SocketIOServer.ts
@@ -2,10 +2,11 @@ import SocketIO from 'socket.io';
 import { Container } from 'typedi';
 import GameService from '../services/GameService';
 import { Invitation } from '../Interfaces/SocketIOServer';
+import { app } from '../index';
 
 const gameService = Container.get(GameService);
 
-const io: SocketIO.Server = SocketIO();
+const io: SocketIO.Server = SocketIO(app);
 
 export default class Server {
     port: number;
@@ -22,102 +23,102 @@ export default class Server {
     }
 
     start(): void {
-      io.on('connection',
-        (baseSocket: SocketIO.Socket): void => {
-          console.log('username');
-          const { username } = baseSocket.handshake.query;
-          this.connectedUsers[username] = baseSocket;
-
-          baseSocket.on('disconnect',
-            () => {
-              delete this.connectedUsers[username];
-            });
-
-          baseSocket.on('sendInvitation',
-            (invitation: Invitation): void => {
-              console.log(invitation);
-              const playerSocket = this.connectedUsers[invitation.receiver];
-              if (playerSocket) {
-                console.log(playerSocket);
-                io.to(playerSocket).emit('invite',
-                  invitation.namespace);
-                baseSocket.emit('invitationSuccess');
-              } else {
-                console.log('not found');
-                baseSocket.emit('invitationFail');
-              }
-            });
-
-          baseSocket.on('game start',
-            (gameNamespace: string): void => {
-              this.namespaces[gameNamespace] = io.of(`/${gameNamespace}`);
-              this.namespaces[gameNamespace].connectedUsers = {};
-              this.namespaces[gameNamespace].on('connection',
-                async (namespaceSocket: SocketIO.Socket): Promise<void> => {
-                  // Returns null if not enough players to start
-                  const { username } = namespaceSocket.handshake.query;
-
-                  this.namespaces[gameNamespace].connectedUsers[username] = namespaceSocket;
-
-                  const playerResults = await gameService.addToPlayerList(gameNamespace,
-                    username);
-                  if (playerResults.ready) {
-                    namespaceSocket.emit('game ready');
-                  }
-
-                  namespaceSocket.on('disconnect',
-                    async (): Promise<void> => {
-                      namespaceSocket.leave('drawerer');
-                      namespaceSocket.leave('answerer');
-
-                      delete this.namespaces[gameNamespace].connectedUsers[username];
-
-                      const playerResults = await gameService.removeFromPlayerList(gameNamespace,
-                        username);
-                      if (playerResults.playerList.length === 0) {
-                        delete this.namespaces[gameNamespace];
-                      } else if (!playerResults.ready) {
-                        namespaceSocket.emit('waiting for players');
-                      }
-                    });
-
-                  namespaceSocket.on('become drawerer',
-                    (): void => {
-                      namespaceSocket.leave('answerers');
-                      namespaceSocket.join('drawerer');
-                    });
-
-                  namespaceSocket.on('become answerer',
-                    (): void => {
-                      namespaceSocket.leave('drawerer');
-                      namespaceSocket.join('answerers');
-                    });
-
-                  namespaceSocket.on('draw',
-                    (image: JSON): void => {
-                      this.namespaces[gameNamespace].to('answerers').emit('drawed',
-                        image);
-                    });
-
-                  namespaceSocket.on('answer',
-                    (answer: string): void => {
-                      this.namespaces[gameNamespace].to('drawerer').emit('answered',
-                        answer);
-                    });
-
-                  namespaceSocket.on('game end',
-                    (gameNamespace: string): void => {
-                      namespaceSocket.leave('drawerer');
-                      namespaceSocket.leave('answerer');
-                      delete this.namespaces[gameNamespace];
-                      gameService.endGame(gameNamespace);
-                    });
-                });
-            });
-        });
-
       try {
-        io.listen(this.port);
+        io.on('connection',
+          (baseSocket: SocketIO.Socket): void => {
+            console.log('username');
+            const { username } = baseSocket.handshake.query;
+            this.connectedUsers[username] = baseSocket;
+
+            baseSocket.on('disconnect',
+              () => {
+                delete this.connectedUsers[username];
+              });
+
+            baseSocket.on('sendInvitation',
+              (invitation: Invitation): void => {
+                console.log(invitation);
+                const playerSocket = this.connectedUsers[invitation.receiver];
+                if (playerSocket) {
+                  console.log(playerSocket);
+                  io.to(playerSocket).emit('invite',
+                    invitation.namespace);
+                  baseSocket.emit('invitationSuccess');
+                } else {
+                  console.log('not found');
+                  baseSocket.emit('invitationFail');
+                }
+              });
+
+            baseSocket.on('game start',
+              (gameNamespace: string): void => {
+                this.namespaces[gameNamespace] = io.of(`/${gameNamespace}`);
+                this.namespaces[gameNamespace].connectedUsers = {};
+                this.namespaces[gameNamespace].on('connection',
+                  async (namespaceSocket: SocketIO.Socket): Promise<void> => {
+                  // Returns null if not enough players to start
+                    const { username } = namespaceSocket.handshake.query;
+
+                    this.namespaces[gameNamespace].connectedUsers[username] = namespaceSocket;
+
+                    const playerResults = await gameService.addToPlayerList(gameNamespace,
+                      username);
+                    if (playerResults.ready) {
+                      namespaceSocket.emit('game ready');
+                    }
+
+                    namespaceSocket.on('disconnect',
+                      async (): Promise<void> => {
+                        namespaceSocket.leave('drawerer');
+                        namespaceSocket.leave('answerer');
+
+                        delete this.namespaces[gameNamespace].connectedUsers[username];
+
+                        const playerResults = await gameService.removeFromPlayerList(gameNamespace,
+                          username);
+                        if (playerResults.playerList.length === 0) {
+                          delete this.namespaces[gameNamespace];
+                        } else if (!playerResults.ready) {
+                          namespaceSocket.emit('waiting for players');
+                        }
+                      });
+
+                    namespaceSocket.on('become drawerer',
+                      (): void => {
+                        namespaceSocket.leave('answerers');
+                        namespaceSocket.join('drawerer');
+                      });
+
+                    namespaceSocket.on('become answerer',
+                      (): void => {
+                        namespaceSocket.leave('drawerer');
+                        namespaceSocket.join('answerers');
+                      });
+
+                    namespaceSocket.on('draw',
+                      (image: JSON): void => {
+                        this.namespaces[gameNamespace].to('answerers').emit('drawed',
+                          image);
+                      });
+
+                    namespaceSocket.on('answer',
+                      (answer: string): void => {
+                        this.namespaces[gameNamespace].to('drawerer').emit('answered',
+                          answer);
+                      });
+
+                    namespaceSocket.on('game end',
+                      (gameNamespace: string): void => {
+                        namespaceSocket.leave('drawerer');
+                        namespaceSocket.leave('answerer');
+                        delete this.namespaces[gameNamespace];
+                        gameService.endGame(gameNamespace);
+                      });
+                  });
+              });
+          });
+
+        // io.listen(this.port);
         console.log('Socket IO Server started');
       } catch (e) { console.log(e); }
     }
