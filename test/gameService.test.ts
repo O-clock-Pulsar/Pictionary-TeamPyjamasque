a81f1975e53b56d2cc780478fcfb3e3cc817131c
AdamWier <38018787+AdamWier@users.noreply.github.com>
2019-11-26 13:45:24 +0100

    First working test. Closes #21

---
+++ test/gameService.test.ts
@@ -0,0 +1,18 @@
+import { expect } from 'chai';
+import { Container } from 'typedi';
+import chaiInterface from 'chai-interface';
+import GameService from '../src/services/GameService';
+
+
+chai.use(chaiInterface);
+
+const gameService = Container.get(GameService);
+
+describe('Game Service Tests',
+  () => {
+    it('Game service\'s createGame returns an object with IGameServiceResult interface',
+      () => {
+        const result = gameService.createGame('Stan Lee');
+        expect(result).to.be.a.instanceOf(Object);
+      });
+  });
