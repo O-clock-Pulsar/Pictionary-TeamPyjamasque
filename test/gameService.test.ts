5f85000d4f868bbe4ba629fd4fbfa0438d59436e
simonMARTIN87 <simon@oclock.io>
2019-11-26 11:04:38 +0100

    working test

---
+++ test/gameService.test.ts
@@ -0,0 +1,24 @@
+// const should = require('chai');
+// const Container = require('typedi');
+// const GameService = require('../src/services/GameService');
+
+// const IGameServiceResult = rewire('src/Interfaces/GameService');
+
+
+import { expect } from 'chai';
+import {Container } from 'typedi';
+import GameService from '../src/services/GameService';
+
+
+// chai.use(require('chai-interface'));
+
+const gameService = Container.get(GameService);
+
+describe('',
+  () => {
+    it('Game service\'s createGame returns an object with IGameServiceResult interface',
+      () => {
+        const result = gameService.createGame('Stan Lee');
+        expect(result).to.be.a.instanceOf(Object);
+      });
+  });
