fa60211f891f4345b4d2bb73f5cb649c4e685f99
AdamWier <38018787+AdamWier@users.noreply.github.com>
2019-12-01 02:31:10 +0100

    Added user integration test for matching passwords. Closes #21

--- test/service/userService.test.ts
+++ test/service/userService.test.ts
@@ -1,18 +1,24 @@
-/*
- * import { expect } from 'chai';
- * import { Container } from 'typedi';
- * import UserService from '../../src/services/UserService';
- */
+import chai, { expect } from 'chai';
+import { Container } from 'typedi';
+import sinon from 'sinon';
+import UserService from '../../src/services/UserService';
+import User from '../../src/models/User';
 
-// const userService = Container.get(UserService);
+const userService = Container.get(UserService);
 
-/*
- * describe('User Service Tests',
- *   () => {
- *     it('User service\'s authenticateUser finds and authenticates user even if username does not have the same case',
- *       async () => {
- *         const { error } = await userService.authenticateUser('Test', 'test');
- *         expect( error ).to.be.false;
- *       });
- *   });
- */
+// Copied from game service tests so it might need other changes
+describe('UserService Tests',
+  () => {
+    it('User service\'s createUser does not create a User if it does not have all the required information',
+      async () => {
+        sinon.stub(User.prototype,
+          'save').returnsThis();
+
+        const result = await userService.createUser({
+          username: 'YourMom', email: 'yourmom@yourmom.com', password: 'ok', confirmation: '',
+        });
+        expect(result).to.have.property('error',
+          true);
+        sinon.restore();
+      });
+  });
